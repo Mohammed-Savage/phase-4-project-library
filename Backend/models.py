336883ba86931c722ae5e43cee6719b35a7f50ab
Tiffany <tiffanyman19@gmail.com>
2024-03-19 09:06:07 -0400

    model

---
+++ Backend/models.py
@@ -0,0 +1,37 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
+import string, datetime
+
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
+db = SQLAlchemy(metadata=metadata)
+
+class Transaction(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key = True)
+    isbn = db.Column(db.String, db.ForeignKey("books.isbn"))
+    member_id = db.Column(db.String, db.ForeignKey("members.id"))
+    burrowed_on = db.Column(db.String, nullable = False)
+    fee_per_day = db.Column(db.String, nullable = False)
+    serialize_rules = ['-book.transactions','-member.transactions'] 
+    book = db.relationship("Books", back_populates='transactions')
+    member = db.relationship("Member", back_populates='transactions')
+
+class Member(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String, nullable = False)
+    email = db.Column(db.String, nullable = False)
+    serialize_rules = ['-transactions.member']
+    transactions= db.relationshipp("Transaction", back_populates= "member")
+
+class Books(db.Model, SerializerMixin):
+    isbn = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String, nullable = False)
+    author = db.Column(db.String, nullable = False)
+    serialize_rules = ['-transactions.book']
+    transactions= db.relationshipp("Transaction", back_populates= "book")
