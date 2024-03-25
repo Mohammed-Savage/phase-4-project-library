454263d86bfd8032557b511134e6b2cc48a809d9
Austin Rivera <austin.rivera.24@aol.com>
2024-03-25 11:53:59 -0400

    add image folder

--- frontend/library/src/pages/Members.jsx
+++ frontend/library/src/pages/Members.jsx
@@ -1,14 +1,35 @@
-import NavBar from "../components/NavBar"
+import NavBar from "../components/NavBar";
+import { useEffect, useState } from "react";
 
-export default function Members() {
-    return(
-        <>
-            <header>
-                <NavBar></NavBar>
-            </header>
-            <main id="members">
-                <h1> Insert Members here </h1>
-            </main>
-        </>
-    )
-}
+const Members = () => {
+  const [members, setMembers] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch("http://localhost:3000/members");
+        const data = await response.json();
+        // Update state with fetched data
+        setMembers(data);
+      } catch (error) {
+        console.error("Error fetching data", error);
+        throw error;
+      }
+    }
+
+    fetchData(); // Call the async function
+  }, []); // Empty dependency array to run only once
+
+  return (
+    <>
+     <NavBar/>
+     <div className="container">
+        <div className="member-card">
+
+        </div>
+     </div>
+    </>
+  );
+};
+
+export default Members;
